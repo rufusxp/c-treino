caeb661683180aaebd122a354e7f0aa08e5b280b
JhonRufus <rufusxp3@gmail.com>
2022-12-27 19:37:01 -0300

    trabalhando com Funções

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -10,36 +10,26 @@ namespace HelloWord
     internal class Program
     {
         static void Main(string[] args)
-        {   Console.WriteLine("Digiti sua idade :");
-            int idade = int.Parse(Console.ReadLine());
-
-            if (idade >= 0 && idade <= 11)
-            {
-                Console.WriteLine("Você é uma criança !");
-            }
-
-            else if (idade >= 12 && idade <= 18)
-            {
-                Console.WriteLine("Você é um adolecente !");
-            }
-
-            else if (idade >= 19 && idade <= 55)
-            {
-                Console.WriteLine("Você é um adulto !");
-            }
-            
-            else if (idade >= 56 && idade <= 100)
-            {
-                Console.WriteLine("Você é um idoso(a) !");
-            }
-            else
-            {
-                Console.WriteLine("Coloque sua idade ");
-            }
-            
+        {   
+            GerarPreco(50);
+            GerarPreco(40);
+            GerarPreco(30);
+            GerarPreco(-10);
+            GerarPreco(-1000);
+            GerarPreco(-10454);
             Console.ReadLine();
 
-
         }
+
+        static void GerarPreco(int preco )
+        {  
+            int precoAbs = Math.Abs(preco);// Match.Abs converte o valor negativo para positivo automaticamente
+            int valorFinal = precoAbs + (2 * precoAbs); 
+            Console.WriteLine("Valor final é :" + valorFinal);
+                
+
+         }
+
+
     }
 }
