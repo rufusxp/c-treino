998c042729c83d8ab1b01d5d72e2bce8421b5e09
JhonRufus <rufusxp3@gmail.com>
2022-12-27 16:27:48 -0300

    primeiro contanto com variaveis !

---
+++ HelloWord/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int segundaGuerraMundial = 1942;
+            float velocidadeDoTanque = 150.45f;
+            string corDoTanque = "verde";
+            bool teveSegundaGuerra = true;
+
+
+            Console.WriteLine(segundaGuerraMundial);
+            Console.WriteLine(velocidadeDoTanque);
+            Console.WriteLine(corDoTanque);
+            Console.WriteLine(teveSegundaGuerra);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
