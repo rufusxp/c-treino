6b51868d9e5de7fba8d2058d1a4a59cf13afeee0
JhonRufus <rufusxp3@gmail.com>
2022-12-27 18:45:19 -0300

    conceito de operadores logicos

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -11,11 +11,27 @@ namespace HelloWord
     {
         static void Main(string[] args)
         {   
+           // tabelinha das condições para ( && ) " e " 
+            // TRUE && TRUE -> true 
+            // FALSE  && FALSE -> false 
+            // FALSE && TRUE -> false 
+            // TRUE && FALSE -> false 
+
+             // tabelinha das condições para ( || ) " OU " 
+            // TRUE && TRUE -> true 
+            // FALSE  && FALSE -> false 
+            // FALSE && TRUE -> true 
+            // TRUE && FALSE -> true 
+            
+            
+            
+            
            int a = 10 ;
            int b = 20 ;
            int c = 2 ;
+ 
             
-            if (a < b) 
+            if (a > b || c < b ) 
             {
                 Console.WriteLine("É VERDADE!! ");
             }
